9dd03f1ab787e5a17d455aaa6743c1d1297297b1
LHS 1140b <LHS1140b@users.noreply.github.com>
2017-07-18 22:32:20 +0200

    Standalone Orbie

---
+++ StandOrbie/StandOrbie/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StandOrbie
+{
+    using Aimtec.SDK.Events;
+    using Aimtec.SDK.Menu;
+    using Aimtec.SDK.Orbwalking;
+
+    class Program
+    {
+        public static Menu Menu = new Menu("Orbwalker", "Orbwalker", true);
+        public static Orbwalker Orbwalker = new Orbwalker();
+
+        static void Main(string[] args)
+        {
+            GameEvents.GameStart += () => Orbwalker.Attach(Menu);
+        }
+    }
+}
